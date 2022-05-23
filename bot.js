da601752e96cf61b8f99ce576a0b134421522418
elphibra <105767375+elphibra@users.noreply.github.com>
2022-05-23 21:13:52 +1000

    publish

---
+++ bot.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+const bot = async() =>{
+    const browser = puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
+    const page = await (await browser).newPage();
+    await page.goto('https://google.com', {
+        waitUntil: 'networkidle2'
+    });
+    await await browser.close();
+    return 'Done';
+}
+
+module.exports = bot;
